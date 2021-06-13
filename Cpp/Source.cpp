05aebc2778db694d3faf28dac59124c86c78cf1c
Gia Huy <nguyengiahuy16@gmail.com>
2021-06-13 22:19:22 +0700

    test graphics 3d

--- Cpp/Source.cpp
+++ Cpp/Source.cpp
@@ -13,6 +13,12 @@
 #include "AppProcess.h"
 #include "Timer.h"
 
+#define DEBUG_GRAPHICS
+
+#ifdef DEBUG_GRAPHICS
+#include "VertextDemo.h"
+#endif
+
 #if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
 #pragma comment(lib, "legacy_stdio_definitions")
 #endif
@@ -33,17 +39,21 @@ int main(int, char**)
     const char* glsl_version = "#version 130";
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+
+#ifdef DEBUG_GRAPHICS
+    // fn
+    demoGenerationColor();
 
     // get monitor
-    //GLFWmonitor* monitor = glfwGetPrimaryMonitor();
-    //if (!monitor) {
-    //    return 0;
-    //}
-    //const int width = glfwGetVideoMode(monitor)->width;
-    //const int height = glfwGetVideoMode(monitor)->height;
-
+    GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+    if (!monitor) {
+        return 0;
+    }
+    const int width = glfwGetVideoMode(monitor)->width;
+    const int height = glfwGetVideoMode(monitor)->height;
+    glm::u32vec2 gameWindowSize(width, height);
+    glm::u32vec2 gameWindowPosition(0, 0);
+#else
     // get process game
     AppProcess* p = AppProcess::Create(_T("Call of Duty®: Modern Warfare® 3"));
     uintptr_t gameBaseAddress = p->getBaseAddress();
@@ -53,12 +63,16 @@ int main(int, char**)
     }
     glm::u32vec2 gameWindowSize = p->getWindowSize();
     glm::u32vec2 gameWindowPosition = p->getWindowPosition();
+#endif
 
     // hint
+    glfwWindowHint(GLFW_SAMPLES, 4);
     glfwWindowHint(GLFW_FLOATING, true);
     glfwWindowHint(GLFW_RESIZABLE, false);
-    //glfwWindowHint(GLFW_MAXIMIZED, true);
     glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, true);
+#ifdef DEBUG_GRAPHICS
+    glfwWindowHint(GLFW_MAXIMIZED, true);
+#endif
 
     // Create window with graphics context
     GLFWwindow* window = glfwCreateWindow(gameWindowSize.x, gameWindowSize.y, "Dear ImGui GLFW+OpenGL3 example", NULL, NULL);
@@ -68,6 +82,7 @@ int main(int, char**)
     // Attr
     glfwSetWindowPos(window, gameWindowPosition.x, gameWindowPosition.y);
     glfwSetWindowAttrib(window, GLFW_DECORATED, false);
+    glfwSetWindowAttrib(window, GLFW_MOUSE_PASSTHROUGH, true);
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1); // Enable vsync
 
@@ -83,36 +98,17 @@ int main(int, char**)
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 
     // Setup Dear ImGui style
     ImGui::StyleColorsDark();
-    //ImGui::StyleColorsClassic();
 
     // Setup Platform/Renderer backends
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init(glsl_version);
+    glEnable(GL_DEPTH_TEST);
 
-    // goooooo!
-    bool visibleMenu = true;
-    glfwSetWindowAttrib(window, GLFW_MOUSE_PASSTHROUGH, !visibleMenu);
-
-    struct {
-        glm::fvec3 p, v1, v2, v3;
-    } data;
-    union {
-        struct
-        {
-            glm::fvec4 a, b, c, d;
-        };
-        glm::fmat4 mat;
-    } matrixView;
-    glm::mat4 mProjection = glm::perspective(45.0f, (GLfloat)gameWindowSize.x / (GLfloat)gameWindowSize.y, 1.0f, 15000.0f);
-    uintptr_t addressPlayer;
-    if (!p->readProcessMemory(gameBaseAddress + address::EBase::Player, addressPlayer)) {
-        return 0;
-    }
+    // Goooooo!
+    bool visibleMenu = false;
 
     // Main loop
     while (!glfwWindowShouldClose(window))
@@ -130,58 +126,62 @@ int main(int, char**)
             glfwSetWindowAttrib(window, GLFW_MOUSE_PASSTHROUGH, !visibleMenu);
         }
 
-        // read data
-        if (!p->readProcessMemory(gameBaseAddress + address::ViewMatrix, data)) {
-            std::cout << "Dont find view matrix!" << std::endl;
-            return 0;
-        }
-
-        //
-        glm::vec3 cameraPos = data.p;
-        glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
-        glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
-        matrixView.mat = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
-
         if (visibleMenu) {
-            ImGui::Text(glm::to_string(data.p).c_str());
-            ImGui::Text(glm::to_string(matrixView.a).c_str());
-            ImGui::Text(glm::to_string(matrixView.b).c_str());
-            ImGui::Text(glm::to_string(matrixView.c).c_str());
-            ImGui::Text(glm::to_string(matrixView.d).c_str());
-
             if (ImGui::Button("Test")) {
                 return 0;
             }
-        }        
+        }
 
         // Rendering
         ImGui::Render();
         int display_w, display_h;
         glfwGetFramebufferSize(window, &display_w, &display_h);
         glViewport(0, 0, display_w, display_h);
-        glEnable(GL_DEPTH_TEST);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glLoadIdentity();
 
-        glColor3f(1.0f, 1.0f, 0);
-
-        glm::fvec4 positionPlayer(0, -1.0f, 0, 0);
-        for (int i = 0; i < 14; i++) {
-            uintptr_t entityAddress;
-            if (p->readProcessMemory(gameBaseAddress + address::Entity + address::Next * i, entityAddress)) {
-                glm::vec3 positionEntity;
-                if (p->readProcessMemory(entityAddress + address::EntityVec3, positionEntity)) {
-                    glm::vec4 screenReal = mProjection * matrixView.mat * glm::vec4(positionEntity, 1.0f);
-                    std::cout << glm::to_string(positionEntity) << " vs " << glm::to_string(screenReal) << std::endl;
-                    glBegin(GL_LINES);
-                    glVertex3fv((float*)&positionPlayer);
-                    glVertex3fv((float*)&screenReal);
-                    glEnd();
-                }
-            }
+#ifdef DEBUG_GRAPHICS
+        if (GetAsyncKeyState(VK_ESCAPE) & 1) {
+            return 0;
         }
+        //std::cout << "-----------" << std::endl;
+        static float rotate = 0;
+        rotate += 0.01f;
 
+        const float aspect = (float)gameWindowSize.x / (float)gameWindowSize.y;
+        const float fnear = -2.0f;
+        const float ffar = 10.0f;
+        glm::mat4 projection = glm::perspective(glm::radians(45.0f), aspect, fnear, ffar);
+        glm::mat4 view = glm::lookAt(glm::vec3(0,0, -10.0f), glm::vec3(0, 0, 0), glm::vec3(0, -1.0f, 0));
+        glm::mat4 model = glm::rotate(glm::mat4(1.0f), rotate, glm::fvec3(1.0f, 0.5f, 0.25f));
+        glDepthRange(fnear, ffar);
 
+        glLineWidth(3.0f);
+        glBegin(GL_TRIANGLES);
+        for (int i = 0; i < sizeof(demoVertices) / sizeof(float) / 3; i++) {
+            const glm::vec3 vertex = *(glm::fvec3*)&demoVertices[i * 3];
+            glColor3ubv(&demoColors[i * 3]);
+
+            const glm::vec4 p1 = projection * view * model * glm::vec4(vertex, 1.0f);
+            if (p1.w < 0.1f) {
+                continue;
+            }
+
+            // ndc
+            const glm::vec3 pNdc = glm::fvec3(p1) / p1.w;
+
+            // screen cood
+            glm::vec3 screenPos(1.0f);
+            screenPos.x = 1 / 2.0f * pNdc.x + 0 + 1 / 2.0f;
+            screenPos.y = 1 / 2.0f * pNdc.y + 0 + 1 / 2.0f;
+            screenPos.z = ((fnear - ffar) / 2.0f * pNdc.z + fnear + ffar) / 2.0f;
+
+            glVertex3fv((float*)&screenPos);
+
+            //std::cout << glm::to_string(vertex) << " -> " << glm::to_string(p1) << " -> " << glm::to_string(pNdc) << " -> " << glm::to_string(screenPos) << std::endl;
+        }
+        glEnd();
+#endif // DEBUG_GRAPHICS
 
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         glfwSwapBuffers(window);
---
+++ Cpp/VertextDemo.h
@@ -0,0 +1,56 @@
+#pragma once
+
+float demoVertices[] = {
+    -1.0f,-1.0f,-1.0f, // triangle 1 : begin
+    -1.0f,-1.0f, 1.0f,
+    -1.0f, 1.0f, 1.0f, // triangle 1 : end
+    1.0f, 1.0f,-1.0f, // triangle 2 : begin
+    -1.0f,-1.0f,-1.0f,
+    -1.0f, 1.0f,-1.0f, // triangle 2 : end
+    1.0f,-1.0f, 1.0f,
+    -1.0f,-1.0f,-1.0f,
+    1.0f,-1.0f,-1.0f,
+
+    1.0f, 1.0f,-1.0f,
+    1.0f,-1.0f,-1.0f,
+    -1.0f,-1.0f,-1.0f,
+
+    -1.0f,-1.0f,-1.0f,
+    -1.0f, 1.0f, 1.0f,
+    -1.0f, 1.0f,-1.0f,
+
+    1.0f,-1.0f, 1.0f,
+    -1.0f,-1.0f, 1.0f,
+    -1.0f,-1.0f,-1.0f,
+
+    -1.0f, 1.0f, 1.0f,
+    -1.0f,-1.0f, 1.0f,
+    1.0f,-1.0f, 1.0f,
+
+    1.0f, 1.0f, 1.0f,
+    1.0f,-1.0f,-1.0f,
+    1.0f, 1.0f,-1.0f,
+
+    1.0f,-1.0f,-1.0f,
+    1.0f, 1.0f, 1.0f,
+    1.0f,-1.0f, 1.0f,
+
+    1.0f, 1.0f, 1.0f,
+    1.0f, 1.0f,-1.0f,
+    -1.0f, 1.0f,-1.0f,
+
+    1.0f, 1.0f, 1.0f,
+    -1.0f, 1.0f,-1.0f,
+    -1.0f, 1.0f, 1.0f,
+
+    1.0f, 1.0f, 1.0f,
+    -1.0f, 1.0f, 1.0f,
+    1.0f,-1.0f, 1.0f
+};
+unsigned char demoColors[sizeof(demoVertices) / sizeof(float)];
+
+void demoGenerationColor() {
+    for (int i = 0; i < sizeof(demoVertices) / sizeof(float); i++) {
+        demoColors[i] = rand() % 255;
+    }
+}
